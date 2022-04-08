5acafdde57656b4a4cbf5738157cda9e568bd466
Amanda-Ruzza <amanda.ruzza@cloudreach.com>
2022-04-08 12:23:16 -0400

    Fixed bugs

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "i-hope-this-works-454545"
-    key    = "projects/ansible/ec2/terraform.tfstates"
+    key    = "projects/ansible-demo/terraform.tfstates"
     #dynamodb_table = "terraform-lock"
   }
 }
